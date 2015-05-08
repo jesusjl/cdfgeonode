8793f97c290bd1e1261d7088538e726bda72d195
Jesus <gesusjl@gmail.com>
2015-05-08 15:22:45 +0000

    change template context processor config to resolve template issue and move debug toolbar to local_settings

--- cdfgeonode/settings.py
+++ cdfgeonode/settings.py
@@ -88,27 +88,10 @@ INSTALLED_APPS = INSTALLED_APPS + (
 )
 
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.request",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages",
+TEMPLATE_CONTEXT_PROCESSORS = TEMPLATE_CONTEXT_PROCESSORS + (
+   
     "sekizai.context_processors.sekizai",
 )
 
-
-INTERNAL_IPS = ('127.0.0.1','192.168.50.1','0.0.0.0')
-
-DEBUG_TOOLBAR_CONFIG = {
-'INTERCEPT_REDIRECTS': False,
-'DISABLE_PANELS': [
-        'debug_toolbar.panels.redirects.RedirectsPanel',
-    ],
-    'SHOW_TEMPLATE_CONTEXT': True,
-}
 
 
