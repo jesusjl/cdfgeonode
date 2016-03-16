8caa0b905cd593a96b0e2d8428807b8383d04e73
Jesus <gesusjl@gmail.com>
2016-03-16 20:57:33 +0000

    fixed url issues and menu, include geonode pages in menu

--- cdfgeonode/urls.py
+++ cdfgeonode/urls.py
@@ -1,24 +1,22 @@
 from django.conf.urls import patterns, url
-
 from geonode.urls import *
+# from django.conf.urls.i18n import i18n_patterns
 
 from wiki.urls import get_pattern as get_wiki_pattern
 from django_nyt.urls import get_pattern as get_nyt_pattern
 
-urlpatterns = patterns('',
-
+# urlpatterns +=  i18n_patterns('',
+urlpatterns +=  patterns('',
     # Static pages
 #    url(r'^$', 'polls.views.index', name='index'),
      url(r'^demo/$', 'demo.views.index'),
 	(r'^notifications/', get_nyt_pattern()),
     #  url(r'^/$',TemplateView.as_view(template_name='base.html'),name='home'),
-          url(r'^$',  include('cms.urls'),name='home'),
-
 	(r'^wiki/_accounts/sign-up/$','demo.views.index'),
-    	(r'^wiki/', get_wiki_pattern()),
+    (r'^wiki/', get_wiki_pattern()),
+    url(r'^',  include('cms.urls')),
 
-
- ) + urlpatterns
+ )
 
 
 if settings.DEBUG:
