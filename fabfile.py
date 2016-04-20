2dca6f81b4b033be2a16912c1544288e8044910e
Jesus <gesusjl@gmail.com>
2016-04-20 14:25:05 +0000

    change requirements and fabfile

--- fabfile.py
+++ fabfile.py
@@ -7,8 +7,11 @@ from fabric.api import *
 import os
 import shutil
 
+# env.shell = "/bin/bash -l -i -c"
+
 env.directory = os.path.dirname(os.path.abspath(__file__))
-env.project_name = 'cdfgeonode'
+env.project_name = 'geonode'
+env.activate = 'source /home/vagrant/.venvs/geonode/bin/activate'
 
 #server
 env.hosts = ['vagrant@192.168.1.29']
@@ -24,16 +27,19 @@ production()
 
 def install_requirements():
     with cd(env.directory):
-        run("workon %s && pip install -r requirements.txt" % (env.project_name))
+        with prefix(env.activate):
+            run("pip install -r requirements.txt")
 
 def update_from_repository():
     with cd(env.directory):
+        run('git checkout %s' % env.branch)
         run('git pull')
 
 def update_database():
     with cd(env.directory):
-        run("workon %s && python manage.py syncdb -all" % env.project_name)
-        run("workon %s && python manage.py migrate -fake" % env.project_name)
+        with prefix(env.activate):
+            run("python manage.py syncdb" % env.project_name)
+            run("python manage.py migrate" % env.project_name)
 
 def init_local():
     install_requirements()
@@ -41,16 +47,18 @@ def init_local():
 
 def collect_static():
     with cd(env.directory):
-        run("workon %s && python manage.py collectstatic --noinput" % (env.project_name))
+        with prefix(env.activate):
+            run("python manage.py collectstatic --noinput")
 
-def restart_webfaction():
+def restart_web():
     run("service apache2 restart", with_sudo=True)
 
 @task(default=True)
 def deploy(mode=None):
     update_from_repository()
-    collect_static()
+
     if mode == 'full':
         install_requirements()
         update_database()
-    restart_webfaction()
+    collect_static()
+    restart_web()
